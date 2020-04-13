6e725970ccd6e31097a5dff723865763e402021d
kilcmn <kilcmn3@hotmail.com>
2020-04-13 11:12:16 -0400

    fetch api and save to database

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,40 @@
+require "http"
+
+API_KEY = ENV["YELP_API_KEY"]
+
+API_HOST = "https://api.yelp.com"
+SEARCH_PATH = "/v3/businesses/search"
+
+DEFAULT_TERM = "dinner"
+DEFAULT_LOCATION = "New York"
+SEARCH_LIMIT = 20
+
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :comments, through: :users
   has_many_attached :image
   
-  def save_database(datas)
-      datas.each do |data|
-        
-      end
+  def self.fetch_restaurants(params)
+    location = DEFAULT_LOCATION
+    if params[:q] 
+      term = params[:q]
+    end  
+      url = "#{API_HOST}#{SEARCH_PATH}"
+      params = {
+        term: term,
+        location: location,
+        limit: SEARCH_LIMIT
+      }
+      response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
+      datas = response.parse["business"]
+      self.save_database(datas)
+      return datas
   end
+
+  def self.save_database(restaurants)
+    restaurants.each do |restaurant|
+      Restaurant.find_or_create_by(name: restaurant[:name])
+    end
+  end
+  
 end
