5a9a667c6b1fb26a6d8dba01571f9582541e3cc1
kilcmn <kilcmn3@hotmail.com>
2020-04-15 02:51:36 -0400

    sign up done

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.1'
 
 gem "http"
-gem 'faker', '~> 2.11'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 gem 'pg', '~> 1.2', '>= 1.2.3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
+    def create
+        user = User.find_by(email: params["users"]["email"])
+        if !user
+            User.create(params_users)
+            render json: {
+                status: 200
+            }
+        elsif 
+            render json: {
+                errors: user.erros.full_messages
+            }
+        end
+    end
 
+    private
+
+    def params_users
+        params.require(:users).permit(:email , :password )
+    end
 end
