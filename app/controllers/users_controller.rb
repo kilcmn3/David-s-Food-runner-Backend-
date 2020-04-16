6965e19bf51aabd384c91936077696dda7bd3f1b
kilcmn <kilcmn3@hotmail.com>
2020-04-16 16:42:07 -0400

    fixing issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,10 +27,23 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        user = User.find_by(id: params["q"])
+        duplicate = User.find_by(email: params["users"]["email"])
+        if !duplicate
+            User.update(params_users)
+            render json: {
+                status: 200
+            }
+        elsif 
+            render json: {
+                errors: user.erros.full_messages
+            }
+        end
+    end
     private
 
     def params_users
         params.require(:users).permit(:email , :password )
     end
-
 end
